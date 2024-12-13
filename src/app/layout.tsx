ce4502b45e135b5ea029e2dc11c353e9c83db635
Peter Mangialardi <petermang122@gmail.com>
2024-12-13 18:11:47 -0500

    sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import "~/styles/globals.css";
-
 import { Space_Mono } from "next/font/google";
 import { type Metadata } from "next";
-
 import { TRPCReactProvider } from "~/trpc/react";
 import {
   ClerkProvider,
@@ -11,6 +9,8 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "~/app/_components/app-sidebar";
 
 export const metadata: Metadata = {
   title: "perfl.io",
@@ -29,18 +29,38 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ClerkProvider>
-      <html lang="en" className={spaceMono.className}>
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <TRPCReactProvider>{children}</TRPCReactProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" className={spaceMono.className} suppressHydrationWarning>
+      <body className="min-h-screen w-full">
+        <ClerkProvider>
+          <SidebarProvider>
+            <div className="flex h-screen w-full overflow-hidden">
+              <SignedIn>
+                <AppSidebar />
+              </SignedIn>
+
+              <div className="flex flex-1 flex-col">
+                <header className="flex items-center justify-between border-b p-4">
+                  <SignedIn>
+                    <SidebarTrigger />
+                  </SignedIn>
+                  <div>
+                    <SignedOut>
+                      <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                      <UserButton afterSignOutUrl="/sign-in" />
+                    </SignedIn>
+                  </div>
+                </header>
+
+                <main className="flex-1 overflow-y-auto">
+                  <TRPCReactProvider>{children}</TRPCReactProvider>
+                </main>
+              </div>
+            </div>
+          </SidebarProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
