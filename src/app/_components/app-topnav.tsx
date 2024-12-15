63bbeb6b2d24c9a1c60237d6517d9da53181cede
Peter Mangialardi <petermang122@gmail.com>
2024-12-15 16:48:36 -0500

    nav coloring

--- src/app/_components/app-topnav.tsx
+++ src/app/_components/app-topnav.tsx
@@ -3,7 +3,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export function AppTopnav() {
   return (
-    <header className="flex items-center justify-between border-b p-4">
+    <header className="bg-sidebar-background flex items-center justify-between p-4">
       <SidebarTrigger />
       <UserButton afterSignOutUrl="/sign-in" />
     </header>
