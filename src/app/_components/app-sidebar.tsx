59e67629434d309fbd01b7c8c37c625df8fce893
Peter Mangialardi <petermang122@gmail.com>
2024-12-14 19:08:28 -0500

    Update app-sidebar.tsx

--- src/app/_components/app-sidebar.tsx
+++ src/app/_components/app-sidebar.tsx
@@ -45,7 +45,7 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>perfl.io</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
